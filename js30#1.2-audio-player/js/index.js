a78563400e59f068e28dff3366b9e944382c24ac
artmigalev <art_migalev@yahoo.com>
2024-09-21 12:58:33 +0300

    feat:add src to songs and add listener with btn next21 сен 2024 г. 12:58:33

--- js30#1.2-audio-player/js/index.js
+++ js30#1.2-audio-player/js/index.js
@@ -2,33 +2,45 @@ const audio = document.querySelector("audio");
 const BTN_PLAY = document.querySelector(".play");
 const BTN_NEXT = document.querySelector(".next");
 const BTN_PREV = document.querySelector(".prev");
-
-
-
+const listSrsSongs = [
+  "./assets/audio/beyonce.mp3",
+  "./assets/audio/dontstartnow.mp3",
+  "./assets/audio/Linkin Park - In The End.mp3",
+  "./assets/audio/Linkin Park - Leave Out All The Rest.mp3",
+  "./assets/audio/Linkin Park - Numb.mp3",
+];
+let indexSong = 0;
 
 window.onload = () => {
+  BTN_PLAY.addEventListener("click", (event) => {
+    const click = event.target;
 
-  BTN_PLAY.addEventListener('click',(event)=> {
-    const click = event.target
-
-    if(click.classList.contains('play')){
-      if(!BTN_PLAY.classList.contains('pause')){
-
-        audio.play()
-        BTN_PLAY.classList.toggle('pause')
-      }else{
-        audio.pause()
-        BTN_PLAY.classList.toggle('pause')
+    if (click.classList.contains("play")) {
+      if (!BTN_PLAY.classList.contains("pause")) {
+        audio.play();
+        BTN_PLAY.classList.toggle("pause");
+      } else {
+        audio.pause();
+        BTN_PLAY.classList.toggle("pause");
       }
     }
-  })
+  });
 
-
-
-}
-
-
-
+  BTN_NEXT.addEventListener("click", (event) => {
+    const click = event.target;
+    if (click.classList.contains("next")) {
+      indexSong++;
+      if (indexSong > listSrsSongs.length) {
+        indexSong = 0;
+      }
+      audio.src = listSrsSongs[indexSong];
+      audio.play();
+      if (!BTN_PLAY.classList.contains("pause")) {
+        BTN_PLAY.classList.add("pause");
+      }
+    }
+  });
+};
 
 // audio.onended = () => {
 //   document.querySelector(".play").style.backgroundImage =
