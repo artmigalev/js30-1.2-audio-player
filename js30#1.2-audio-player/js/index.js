e6bd9bceff2f81818522896472af8773b2ed02ae
artmigalev <art_migalev@yahoo.com>
2024-09-22 13:19:06 +0300

    "feat: progress bar , next songs prev songs`date`"

--- js30#1.2-audio-player/js/index.js
+++ js30#1.2-audio-player/js/index.js
@@ -2,19 +2,59 @@ const audio = document.querySelector("audio");
 const BTN_PLAY = document.querySelector(".play");
 const BTN_NEXT = document.querySelector(".next");
 const BTN_PREV = document.querySelector(".prev");
+const artist = document.querySelector(".artist-name");
+const song = document.querySelector(".song-name");
+const display = document.querySelector(".img-artist");
+const currentTime = document.querySelector(".time-start");
+const finishSong = document.querySelector(".time-end");
+const progress = document.querySelector(".progress-bar");
 const listSrsSongs = [
-  "./assets/audio/beyonce.mp3",
-  "./assets/audio/dontstartnow.mp3",
-  "./assets/audio/Linkin Park - In The End.mp3",
-  "./assets/audio/Linkin Park - Leave Out All The Rest.mp3",
-  "./assets/audio/Linkin Park - Numb.mp3",
+  {
+    artist: "Linlin Park",
+    song: "Numb",
+    displayPic: "IL3s8qi8XaA.jpg",
+  },
+  {
+    artist: "Linlin Park",
+    song: "In The End",
+    displayPic: "OBKWpCynBhI.jpg",
+  },
+  {
+    artist: "Linlin Park",
+    song: "Leave Out All The Rest",
+    displayPic: "xP9aGHEGWlc.jpg",
+  },
 ];
 let indexSong = 0;
+audio.load()
+audio.onloadedmetadata = (event) => {
 
+
+  finishSong.innerHTML = convertedToMinutes(Math.trunc(audio.duration));
+  progress.min = audio.currentTime;
+  progress.max = audio.duration;
+  console.log(progress.max);
+
+
+};
+
+
+
+audio.addEventListener("playing", () => {
+  audio.addEventListener("timeupdate", (event) => {
+    currentTime.innerHTML = convertedToMinutes(Math.trunc(audio.currentTime));
+    progress.value = audio.currentTime;
+    progress.addEventListener("click", (event)=>{
+      audio.currentTime = progress.value
+      
+
+
+    });
+  });
+});
 window.onload = () => {
   BTN_PLAY.addEventListener("click", (event) => {
     const click = event.target;
-
     if (click.classList.contains("play")) {
       if (!BTN_PLAY.classList.contains("pause")) {
         audio.play();
@@ -30,19 +70,50 @@ window.onload = () => {
     const click = event.target;
     if (click.classList.contains("next")) {
       indexSong++;
-      if (indexSong > listSrsSongs.length) {
+      if (indexSong > listSrsSongs.length - 1) {
         indexSong = 0;
       }
-      audio.src = listSrsSongs[indexSong];
-      audio.play();
-      if (!BTN_PLAY.classList.contains("pause")) {
-        BTN_PLAY.classList.add("pause");
-      }
+      settingSong(indexSong);
     }
+  });
+  BTN_PREV.addEventListener("click", (event) => {
+    const click = event.target;
+    if (click.classList.contains("prev")) {
+      indexSong--;
+      if (indexSong < 0) {
+        indexSong = listSrsSongs.length - 1;
+      }
+      settingSong(indexSong);
+    }
+  });
+  audio.addEventListener("play", (event) => {
+    BTN_PLAY.classList.add("pause");
+    currentTime.innerHTML = convertedToMinutes(Math.trunc(audio.currentTime));
+  });
+  audio.addEventListener("ended", () => {
+    settingSong(indexSong++);
+    audio.play();
   });
 };
 
-// audio.onended = () => {
-//   document.querySelector(".play").style.backgroundImage =
-//     "linear-gradient(rgba(242, 242, 242, 0.5), rgba(7, 4, 4, 0.5)),url('./assets/svg/play.png')";
-// };
+function settingSong(indexSong) {
+  audio.src = `./assets/audio/Linkin Park - ${listSrsSongs[indexSong].song}.mp3`;
+  audio.addEventListener("loadedmetadata", function () {
+    finishSong.innerHTML = convertedToMinutes(Math.trunc(audio.duration));
+    currentTime.innerHTML = convertedToMinutes(Math.trunc(audio.currentTime));
+  });
+  artist.textContent = listSrsSongs[indexSong].artist;
+  song.textContent = listSrsSongs[indexSong].song;
+  display.src = `./assets/img/player_pic/${listSrsSongs[indexSong].displayPic}`;
+
+  if (BTN_PLAY.classList.contains("pause")) {
+    audio.play();
+  }
+  return;
+}
+
+function convertedToMinutes(s) {
+  let m = Math.trunc(s / 60) + "";
+  s = (s % 60) + "";
+  return m.padStart(2, 0) + ":" + s.padStart(2, 0);
+}
